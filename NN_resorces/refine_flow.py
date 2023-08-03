d20e12e9efb3fba0a082e52da9d7db8c33dbb21a
MauricioSalles <201702053@est.umss.edu>
2023-08-02 20:21:57 -0400

    Refinado de metodo por sectores

--- NN_resorces/refine_flow.py
+++ NN_resorces/refine_flow.py
@@ -1,71 +1,45 @@
 import torch
-import torch.cuda as cuda
 from sklearn.cluster import KMeans
-from torch.nn.functional import pad
-from torch import load,unsqueeze,cat, no_grad
 import torchvision.transforms as transforms
-import argparse
 import cv2
 import numpy as np
-#from torchvision.models.optical_flow import raft_large
-#from torchvision.models.optical_flow import Raft_Large_Weights
-from torch.nn import DataParallel
 from torch import load
-try:
-    from GMA.core.network import RAFTGMA
-    from GMA.core.utils.utils import InputPadder                
-except ImportError:
-        from .GMA.core.network import RAFTGMA
-        from .GMA.core.utils.utils import InputPadder
 
 
 class FlowModule():
     
-    def __init__(self):
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.transform = transforms.ToTensor()
-        #self.model = raft_large(weights=Raft_Large_Weights.C_T_V1).to(self.device)
-        self.args = self.loadArgs()
-        self.flow = DataParallel(RAFTGMA(self.args))
-        self.flow.load_state_dict(load(self.args.model))
-        self.flow = self.flow.module
-        self.flow.to(self.device)
-        self.flow.eval()
-        
-    def loadArgs(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--model', help="restore checkpoint", default="GMA/checkpoints/gma-things.pth")
-        parser.add_argument('--model_name', help="define model name", default="GMA")
-        parser.add_argument('--path', help="dataset for evaluation")
-        parser.add_argument('--num_heads', default=1, type=int,
-                            help='number of heads in attention and aggregation')
-        parser.add_argument('--position_only', default=False, action='store_true',
-                            help='only use position-wise attention')
-        parser.add_argument('--position_and_content', default=False, action='store_true',
-                            help='use position and content-wise attention')
-        parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
-        return parser.parse_args()
-    
-    def segment(self,image):
+    def segment(self,image1,image2):
         #segment image by color range
-        h,w,c = image.shape
+        h,w,c = image1.shape
         color_index = np.zeros((26,26,26),dtype=int)
-        segment_list = []
-        colorAbg = []
+        segment_list1 = []
+        segment_list2 = []
         segment = 1
         for ht in range(h):
             for wt in range(w):
-                b,g,r = (image[ht][wt]+1)//64
-                idx = color_index[b][g][r]
-                if idx == 0:
-                    color_index[b][g][r] = segment
-                    idx = segment
+                b1,g1,r1 = (image1[ht][wt]+1)//64
+                b2,g2,r2 = (image2[ht][wt]+1)//64
+                idx1 = color_index[b1][g1][r1]
+                if idx1 == 0:
+                    color_index[b1][g1][r1] = segment
+                    idx1 = segment
                     segment += 1
-                    segment_list.append(np.zeros_like(image,dtype=int))
-                segment_list[idx-1][ht][wt][0]=b*64
-                segment_list[idx-1][ht][wt][1]=g*64
-                segment_list[idx-1][ht][wt][2]=r*64
-        return segment_list
+                    segment_list1.append(np.zeros_like(image1,dtype=float))
+                    segment_list2.append(np.zeros_like(image1,dtype=float))
+                idx2 = color_index[b2][g2][r2]
+                if idx2 == 0:
+                    color_index[b2][g2][r2] = segment
+                    idx2 = segment
+                    segment += 1
+                    segment_list1.append(np.zeros_like(image1,dtype=float))
+                    segment_list2.append(np.zeros_like(image1,dtype=float))
+                segment_list1[idx1-1][ht][wt][0]=b1*64
+                segment_list1[idx1-1][ht][wt][1]=g1*64
+                segment_list1[idx1-1][ht][wt][2]=r1*64
+                segment_list2[idx2-1][ht][wt][0]=b2*64
+                segment_list2[idx2-1][ht][wt][1]=g2*64
+                segment_list2[idx2-1][ht][wt][2]=r2*64
+        return segment_list1,segment_list2
     
     def segmentCluster(self, image1, image2,  K = 6):
         #segment image by color clusters
@@ -120,53 +94,16 @@ class FlowModule():
                 img1 = cv2.remap(img1, flowWarp,None, cv2.INTER_LINEAR)
             Frame1gray = cv2.cvtColor(np.float32(img1), cv2.COLOR_BGR2GRAY)
             Frame2gray = cv2.cvtColor(np.float32(img2), cv2.COLOR_BGR2GRAY)
-            relativeFlow = cv2.calcOpticalFlowFarneback(Frame1gray, Frame2gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            relativeFlow = cv2.calcOpticalFlowFarneback(Frame1gray, Frame2gray, None, 0.5, 1, 15, 3, 5, 1.2, 0)
             if i > 0:
                 flow = flow + relativeFlow
             else:
                 flow = relativeFlow
         return flow
     
-    def calcFlowGMA(self, frame1, frame3):
-        img1=unsqueeze(self.transform(frame1),0)
-        img3=unsqueeze(self.transform(frame3),0)
-        padX=0
-        padX2=0
-        padY=0
-        padY2=0
-        if w%8 > 0:
-            padX = 8-w%8
-            if padX%2 == 0:
-                padX2 = int(padX/2)
-                padX = int(padX/2)
-            else:
-                padX2 = int(padX/2)
-                padX = int(padX/2+1)
-        if h%8 >0:
-            padY = 8-h%8
-            if padY%2 == 0:
-                padY2 = int(padY/2)
-                padY = int(padY/2)
-            else:
-                padY2 = int(padY/2)
-                padY = int(padY/2+1)
-        img1 = pad(img1, (padY2,padY,padX,padX2), "constant", 0)
-        img3 = pad(img3, (padY2,padY,padX,padX2), "constant", 0)
-        with torch.no_grad():
-            flow_low, output = self.flow(img1.to(self.device), img3.to(self.device), iters=20, test_mode=True)
-        b,c,w,h = output.shape
-        output = output[:,:,padX:w-padX2,padY:h-padY2]
-        return np.transpose(output.cpu().numpy()[0], (1,2,0))
         
-    def calcSegmentFlow(self, img1, img2):
-        #calculate flow of one segment
-        padder = InputPadder(img1.shape)
-        img1, img2 = padder.pad(img1, img2)
-        with torch.no_grad():
-            flow_low, flow_up = self.calcFlowGMA(img1, img2)
-        return flow_up
     
-    def getFlow(self, img1, img2, K=6,segmentType='manual',flowType='manual',levels=1):
+    def getFlow(self, img1, img2, K=6,segmentType='sector',levels=1):
         #generate flow based on the calculation of the flows of the segments
         flow12 = []
         flow21 = []
@@ -174,9 +111,8 @@ class FlowModule():
         segments2 = []
         Flow12 = []
         Flow21 = []
-        if segmentType.__eq__('manual'):
-            segments1 = self.segment(img1)
-            segments2 = self.segment(img2)
+        if segmentType.__eq__('sector'):
+            segments1,segments2 = self.segment(img1,img2)
         if segmentType.__eq__('cluster'):  
             segments1, segments2 = self.segmentCluster(img1,img2, K)
         if segmentType.__eq__('none'):
@@ -185,12 +121,8 @@ class FlowModule():
         for i in range(len(segments1)):
             img1 = segments1[i]
             img2 = segments2[i]
-            if flowType.__eq__('manual'):
-                f1 = self.calcFlow(img1, img2,levels=levels)
-                f2 = self.calcFlow(img2, img1,levels=levels)
-            if flowType.__eq__('GMA'):
-                f1 = self.calcFlowGMA(img1, img2)
-                f2 = self.calcFlowGMA(img2, img1)
+            f1 = self.calcFlow(img1, img2,levels=levels)
+            f2 = self.calcFlow(img2, img1,levels=levels)
             flow12.append((f1))
             flow21.append((f2))
         
--- PrecisionFlow.py
+++ PrecisionFlow.py
@@ -7,11 +7,14 @@ dataset = dt.FramesDataset('dataset')
 len = dataset.__len__()
 flow = FlowModule()
 totalError = 0
+totalErrorCluster = 0
+totalErrorSector = 0
+totalErrorRefine = 0
 
-def calcFrame(frame1 , frame2,flow):
+def calcFrame(frame1 , frame2,flow,segmentType,levels):
     hsv = np.zeros_like(frame1)
     hsv[..., 1] = 255
-    flow,flow2 = flow.getFlow(frame1, frame2,segmentType='none',flowType='manual',levels=2)
+    flow,flow2 = flow.getFlow(frame1, frame2,segmentType=segmentType,levels=levels)
     h, w = flow.shape[:2]
     flow = -flow
     flow[:,:,0] += np.arange(w)
@@ -19,11 +22,22 @@ def calcFrame(frame1 , frame2,flow):
     newFrame = cv2.remap(frame1, flow,None, cv2.INTER_LINEAR)
     return newFrame
 
-print(len)
-for i in range(len):
+
+for i in range(100):
     print(i)
     f1, f2, f3 = dataset.__getitem__(i)
-    fNew = calcFrame(f1,f3,flow)
-    error = np.square(fNew-f2).mean()
-    totalError += error
-print(totalError/len)
+    fNewNormal = calcFrame(f1,f3,flow,segmentType='none',levels=1)
+    fNewCluster = calcFrame(f1,f3,flow,segmentType='cluster',levels=1)
+    fNewSector = calcFrame(f1,f3,flow,segmentType='sector',levels=1)
+    fNewRefine = calcFrame(f1,f3,flow,segmentType='none',levels=2)
+    fNewRefineSector = calcFrame(f1,f3,flow,segmentType='sector',levels=2)
+    totalError = np.str_(np.abs(fNewNormal-f2).mean())
+    totalErrorCluster = np.str_(np.abs(fNewCluster-f2).mean())
+    totalErrorSector = np.str_(np.abs(fNewSector-f2).mean())
+    totalErrorRefine = np.str_(np.abs(fNewRefine-f2).mean())
+    totalErrorRefineSector = np.str_(np.abs(fNewRefineSector-f2).mean())
+print("error original:  "+ totalError/len)
+print("error de metodo de cluster:  "+totalErrorCluster/len)
+print("error de metodo de sectores:  "+totalErrorSector/len)
+print("error de metodo de refinado:  "+totalErrorRefine/len)
+print("error de metodo de refinado y sectores:  "+totalErrorRefineSector/len)
