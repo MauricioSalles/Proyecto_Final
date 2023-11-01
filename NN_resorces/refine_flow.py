b609b7f2411cc430912608cdf58b2b316fc7039f
MauricioSalles <201702053@est.umss.edu>
2023-10-31 20:57:54 -0400

    correccion

--- NN_resorces/refine_flow.py
+++ NN_resorces/refine_flow.py
@@ -9,6 +9,7 @@ class refine_flow():
         Frame1gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
         Frame2gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
         flow = cv2.calcOpticalFlowFarneback(Frame1gray, Frame2gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        h, w = flow.shape[:2]
         flow[:,:,0] += np.arange(w)
         flow[:,:,1] += np.arange(h)[:,np.newaxis]
         return flow
